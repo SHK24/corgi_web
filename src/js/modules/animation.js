67965e7d67122c80bc54e0e3cbdadff55b55dd6b
Liteiniy <liteiniy.dev@gmail.com>
2024-03-19 21:50:53 +0700

    fix: animation script

--- src/js/modules/animation.js
+++ src/js/modules/animation.js
@@ -1,82 +1,29 @@
 const mediaQuery = window.matchMedia('(min-width: 743px)');
 
+function loadAnimation(id, path) {
+  lottie.loadAnimation({
+    container: document.getElementById(id),
+    path,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+  });
+}
+
 function handleTabletChange(e) {
   if (e.matches) {
     lottie.destroy();
-
-    lottie.loadAnimation({
-      name: 'promo-main',
-      container: document.getElementById('promo-lottie-animation'),
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '../files/promo-main.json',
-    });
-
-    lottie.loadAnimation({
-      name: 'promo-main',
-      container: document.getElementById('ai-lottie-animation'),
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '../files/corgi-ai.json',
-    });
-
-    lottie.loadAnimation({
-      name: 'promo-main',
-      container: document.getElementById('language-lottie-animation'),
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '../files/language.json',
-    });
-
-    lottie.loadAnimation({
-      name: 'promo-main',
-      container: document.getElementById('trainer-lottie-animation'),
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '../files/practice.json',
-    });
+    loadAnimation('promo-lottie-animation', 'files/promo-main.json');
+    loadAnimation('ai-lottie-animation', 'files/promo-main.json');
+    loadAnimation('ai-lottie-animation', 'files/corgi-ai.json');
+    loadAnimation('language-lottie-animation', 'files/language.json');
+    loadAnimation('trainer-lottie-animation', 'files/practice.json');
   } else {
     lottie.destroy();
-
-    lottie.loadAnimation({
-      name: 'promo-main',
-      container: document.getElementById('promo-lottie-animation'),
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '../files/promo-main-mobile.json',
-    });
-
-    lottie.loadAnimation({
-      name: 'promo-main',
-      container: document.getElementById('ai-lottie-animation'),
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '../files/corgi-ai-mobile.json',
-    });
-
-    lottie.loadAnimation({
-      name: 'promo-main',
-      container: document.getElementById('language-lottie-animation'),
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '../files/language-mobile.json',
-    });
-
-    lottie.loadAnimation({
-      name: 'promo-main',
-      container: document.getElementById('trainer-lottie-animation'),
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '../files/practice-mobile.json',
-    });
+    loadAnimation('promo-lottie-animation', 'files/promo-main-mobile.json');
+    loadAnimation('ai-lottie-animation', 'files/corgi-ai-mobile.json');
+    loadAnimation('language-lottie-animation', 'files/language-mobile.json');
+    loadAnimation('trainer-lottie-animation', 'files/practice-mobile.json');
   }
 }
 
