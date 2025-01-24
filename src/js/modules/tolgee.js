2835037f13b8777c76f56c40ea0a2ae7c8e20221
Vladimir Barkasov <vbarkasov4dev@gmail.com>
2025-01-25 00:26:49 +0700

    Home page, Privacy policy page: i18n

--- src/js/modules/tolgee.js
+++ src/js/modules/tolgee.js
@@ -12,18 +12,38 @@ const initTolgee = () => {
       // ## remove it in for production publicly accessible site   ##
       // ############################################################
       apiKey: '',
-      apiUrl: '',
+      apiUrl: 'https://app.tolgee.io',
       defaultLanguage: 'en',
       observerType: 'text',
       observerOptions: { inputPrefix: '{{', inputSuffix: '}}' },
     });
 
-  tolgee.run()/*.then(() => {
-    document.getElementById('loading').style.display = 'none';
-  })*/;
+  tolgee.run().then(() => {
+    document.querySelectorAll('[data-i18n-placeholder]').forEach((el) => {
+      const placeholder = tolgee.t(el.dataset.i18nPlaceholder, { noWrap: true })
+      if(placeholder) {
+        el.setAttribute('placeholder', placeholder)
+      }
+    })
+
+    document.querySelectorAll('[data-i18n-alt]').forEach((el) => {
+      const alt = tolgee.t(el.dataset.i18nAlt, { noWrap: true })
+      if(alt) {
+        el.setAttribute('alt', alt)
+      }
+    })
+
+    document.querySelectorAll('[data-i18n]').forEach((el) => {
+      const localizedText = tolgee.t(el.dataset.i18n, { noWrap: true })
+      if(localizedText) {
+        el.textContent = localizedText
+      }
+    })
+  });
 
   tolgee.on('update', () => {
-    document.title = tolgee.t('common_title', { noWrap: true });
+    console.log('Records')
+    console.log([...tolgee.getAllRecords()[0].data].map(item => item[0]))
   });
 }
 
