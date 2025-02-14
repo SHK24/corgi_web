ac93d135f9881c9145d7a9167939761db0100d4a
Vladimir Barkasov <vbarkasov4dev@gmail.com>
2025-02-14 12:47:02 +0700

    Extend dicts and add new langs

--- src/js/modules/tolgee.js
+++ src/js/modules/tolgee.js
@@ -2,8 +2,14 @@ const initTolgee = async () => {
   const { Tolgee, InContextTools, FormatSimple, BackendFetch } =
     window['@tolgee/web'];
 
+  const de = await fetch('/i18n/de.json').then(res => res.json())
   const en = await fetch('/i18n/en.json').then(res => res.json())
   const es = await fetch('/i18n/es.json').then(res => res.json())
+  const fr = await fetch('/i18n/fr.json').then(res => res.json())
+  const ja = await fetch('/i18n/ja.json').then(res => res.json())
+  const ptBR = await fetch('/i18n/pt-BR.json').then(res => res.json())
+  const ru = await fetch('/i18n/ru.json').then(res => res.json())
+  const vi = await fetch('/i18n/vi.json').then(res => res.json())
 
   const tolgee = Tolgee()
     .use(InContextTools())
@@ -14,12 +20,18 @@ const initTolgee = async () => {
       // ## you should never leak your API key                     ##
       // ## remove it in for production publicly accessible site   ##
       // ############################################################
-      apiKey: '',
-      apiUrl: 'https://app.tolgee.io',
+      // apiKey: '',
+      // apiUrl: 'https://app.tolgee.io',
       defaultLanguage: 'en',
       staticData: {
+        de,
         en,
         es,
+        fr,
+        ja,
+        'pt-BR': ptBR,
+        ru,
+        vi,
       }
     });
 
@@ -27,10 +39,10 @@ const initTolgee = async () => {
     langSwitcher.localizeAll()
   });
 
-  tolgee.on('update', () => {
+  /*tolgee.on('update', () => {
     console.log('Records')
     console.log(tolgee.getAllRecords())
-  });
+  });*/
 
   window.tolgee = tolgee
   await langSwitcher.init()
