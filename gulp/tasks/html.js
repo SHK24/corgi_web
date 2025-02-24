d0b45e3ab281f050fba32048c84783a05d1f534b
Vladimir Barkasov <vbarkasov4dev@gmail.com>
2025-02-24 23:58:42 +0700

    Build html files to i18n-directories

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -5,7 +5,20 @@ import webpHtmlNosvg from 'gulp-webp-html-nosvg';
 import versionNumber from 'gulp-version-number';
 import htmlmin from 'gulp-htmlmin';
 
-export const html = () => {
+const version = versionNumber({
+  value: '%DT%',
+  append: {
+    key: '_v',
+    cover: 0,
+    to: ['css', 'js'],
+  },
+  output: {
+    file: 'gulp/version.json',
+  },
+})
+
+export const html = (destSubDirectory='') => {
+  const isRoot = destSubDirectory === ''
   return app.gulp
     .src(app.path.src.html)
     .pipe(
@@ -17,25 +30,13 @@ export const html = () => {
       ),
     )
     .pipe(fileinclude())
-    .pipe(app.plugins.replace(/@img\//g, 'img/'))
+    .pipe(app.plugins.replace(/@img\//g, '/img/'))
     .pipe(app.plugins.if(app.isBuild, webpHtmlNosvg()))
-    .pipe(
-      app.plugins.if(
-        app.isBuild,
-        versionNumber({
-          value: '%DT%',
-          append: {
-            key: '_v',
-            cover: 0,
-            to: ['css', 'js'],
-          },
-          output: {
-            file: 'gulp/version.json',
-          },
-        }),
-      ),
-    )
+    .pipe(app.plugins.if(
+      app.isBuild && isRoot,
+      version,
+    ))
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(app.gulp.dest(app.path.build.html))
+    .pipe(app.gulp.dest(app.path.build.html + destSubDirectory))
     .pipe(app.plugins.browsersync.stream());
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 import gulp from 'gulp';
+import fs from 'fs';
+import * as pathUtil from 'path';
 
 import { path } from './gulp/config/path.js';
 import { plugins } from './gulp/config/plugins.js';
@@ -23,17 +25,46 @@ global.app = {
   plugins,
 };
 
+function getI18nFileNames(sourcePath) {
+  return fs.readdirSync(sourcePath, { withFileTypes: true })
+    .filter(dirent => dirent.isFile() && pathUtil.extname(dirent.name) === '.json')
+    .map(dirent => dirent.name.split('.')[0])
+}
+
+function htmlApply(lang) {
+  return function htmlTask(done) {
+    html(lang);
+    done();
+  };
+}
+
+function htmlForEachLang(done) {
+  const langs = getI18nFileNames('./src/i18n'); // Получаем список языков
+  if (langs.length === 0) {
+    console.log('JSON-файлы в /src/i18n/ не найдены.');
+    return done();
+  }
+  const tasks = [
+    ...langs,
+    '', // root directory
+  ].map(lang => {
+    console.log(`Копирование HTML-файлов для локализации: ${lang ? lang : 'корневая директория'}`);
+    return htmlApply(lang);
+  });
+  return gulp.series(...tasks)(done);
+}
+
 function watcher() {
   gulp.watch(path.watch.files, reset);
   gulp.watch(path.watch.files, copy);
   gulp.watch(path.watch.i18n, i18n);
-  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.html, htmlForEachLang);
   gulp.watch(path.watch.scss, scss);
   gulp.watch(path.watch.js, js);
   gulp.watch(path.watch.img, img);
 }
 
-const mainTasks = gulp.parallel(copy, i18n, robots, ttfToWoff, ttfToWoff2, html, scss, js, img, sprite); //
+const mainTasks = gulp.parallel(copy, i18n, robots, ttfToWoff, ttfToWoff2, htmlForEachLang, scss, js, img, sprite);
 const serverTasks = gulp.parallel(watcher, server);
 
 const dev = gulp.series(reset, mainTasks, serverTasks);
